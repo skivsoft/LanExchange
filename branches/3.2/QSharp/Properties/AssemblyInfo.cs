1e69d532a7b4b3c5ba2d248d8dd7fb5875f5948d
skivsoft@gmail.com <skivsoft@gmail.com@e906daf5-3dc3-8edb-9921-326bb94d082b>
2014-10-24 21:09:52 +0000

    QSharp draft project added

---
+++ branches/3.2/QSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("QSharp")]
+[assembly: AssemblyDescription("User-friendly browser of computers for local area network.")]
+[assembly: AssemblyCopyright("© 2012—2014 Kostya Mikhailyuk")]
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+
+[assembly: NeutralResourcesLanguageAttribute("en-US", UltimateResourceFallbackLocation.MainAssembly)]
